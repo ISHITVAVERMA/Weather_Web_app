69884acea3183707382b462ba9e3179f8374d246
Ishitva Verma <i_shit@Ishitvas-MacBook-Air.local>
2021-09-25 21:53:33 +0530

    final commit

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 const express=require("express");
 const https=require("https");
+const bodyParser=require("body-parser");
 const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/",function(req,res){
-
-  var link="https://api.openweathermap.org/data/2.5/weather?q=london&appid=346b907862ae20b1c42d4ae8b11b127b";
-
-  https.get(link,function(response){
-    console.log(response);
-    response.on("data",function(data)
-    {
-      var whetherdata = JSON.parse(data);
-      var temp = whetherdata.main.temp;
-      console.log(temp);
-    });
-  });
-
-
-
-  res.send("hello there");
+  res.sendFile(__dirname+"/index.html");
 });
 
+app.post("/",function(req,res){
+  var city = req.body.cityName;
+  var link="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=346b907862ae20b1c42d4ae8b11b127b";
+  console.log(city);
+  https.get(link,function(response){
+    //console.log(response);
+    response.on("data",function(data)
+    {
+      const whetherdata = JSON.parse(data);
+      const temp = whetherdata.main.temp;
+      console.log(temp);
+      res.send("<h1>"+temp+"</h1>");
+    });
+  });
+});
+
+//
 app.listen(3000,function(){
   console.log("The port is listening");
 });
